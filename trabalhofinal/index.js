e0dcad2268530f396571099ca706f135accfa3c8
cassiobmarcolino <cassiobentomarcolino@gmail.com>
2023-05-05 19:37:59 -0300

    alteração na autenticacao do login, redirecionando para a pagina do google.

--- trabalhofinal/index.js
+++ trabalhofinal/index.js
@@ -1,6 +1,8 @@
 const form = document.querySelector("#form");
 const campos =  document.querySelectorAll(".required");
 const spans = document.querySelectorAll(".span-required");
+const corpoBody = document.querySelector("#body");
+const btn = document.querySelector(".meubotao");
 
 const regex =/^\S+@\S+\.\S+$/;
 const email = 'otaku@gmail.com';
@@ -16,23 +18,42 @@ function setError(index){
 
 function removeError(index){
     campos[index].style.border = '';
+    spans[index].style.display = 'none';
     
     
 }
 function emailValidate(){
-    if(!regex.test(campos[0].value)){
+    if(!regex.test(campos[0].value || campos[0].value === email)){
         setError(0)
     }else{
         removeError(0);
     }
 }
 
+function senhaValidate(){
+    if(!campos[1].value == senha){
+        setError(1)
+    }else{
+        removeError(1)
+    }
+}
 
 
 function loginValidate(){
-    if(campos[0].value == email && campos[1].value == senha){
-        console.log("validado");
+    if(campos[0].value === email && campos[1].value === senha){
+        window.location.href = 'http://www.google.com'
     }else{
-        alert("Email ou senha errado.")
+        setError(0)
+        setError(1)
+        campos[0].textContent("Email ou Senha Incorreto.");
+        campos[1].textContent("Email ou Senha Incorreto.");
+        
+        
     }
 }
+
+// function submitButao(){
+//     if(campos[0].value === email && campos[1].value === senha){
+//         btn.innerHTML = "button type='submit'";
+//     }
+// }
